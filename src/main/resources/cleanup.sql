fc84a27ebd85ddefdb68d19fc477700e035b006b
Leo Bayer <lfbayer@gmail.com>
2015-02-02 17:38:05 +0900

    update manual sequences

--- src/main/resources/cleanup.sql
+++ src/main/resources/cleanup.sql
@@ -21,3 +21,24 @@ CREATE FUNCTION fixup_sequences() RETURNS void AS $$
 $$ LANGUAGE plpgsql;
 SELECT fixup_sequences();
 DROP FUNCTION fixup_sequences();
+
+CREATE FUNCTION fix_up_manual_sequences() RETURNS void AS $$
+  DECLARE
+    target_seq RECORD;
+    max_result RECORD;
+  BEGIN
+    FOR target_seq IN SELECT s.relname as seq, substr(s.relname, 0, length(s.relname) - 3) AS tab
+      FROM pg_class s
+      LEFT JOIN pg_depend d on d.objid=s.oid and d.classid='pg_class'::regclass and d.refclassid='pg_class'::regclass
+      WHERE s.relkind='S' AND d.objid IS NULL
+    LOOP
+      FOR max_result IN EXECUTE 'SELECT cast((coalesce(MAX(id), 0)+1) as BIGINT) as last FROM ' || quote_ident(target_seq.tab)
+      LOOP
+        RAISE NOTICE 'new sequence value for column %.id: %', target_seq.tab, max_result.last;
+        EXECUTE 'SELECT setval(''' || quote_ident(target_seq.seq) || ''', ' || max_result.last || ', false)';
+      END LOOP;
+    END LOOP;
+  END;
+$$ LANGUAGE plpgsql;
+SELECT fix_up_manual_sequences();
+DROP FUNCTION fix_up_manual_sequences();
